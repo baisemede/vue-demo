e2447170970398a00b2801088a4870e4df129b13
sujimao <1046975490@qq.com>
2019-01-27 20:57:01 +0800

    添加加载状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-    <g-icon v-if="icon" :name="icon"></g-icon>
+    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name='loading'></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -21,6 +22,10 @@ export default {
 };
 </script>
 <style lang='scss'>
+@keyframes spin{
+  0%{transform: rotate(0deg);}
+  100%{transform: rotate(360deg);}
+}
 .g-button {
   font-size: var(--font--size);
   height: var(--button-height);
@@ -57,6 +62,9 @@ export default {
       order: 2;
       margin-right: 0;
       margin-left: 0.1em;
+    }
+    .loading{
+      animation: spin 2.5s infinite linear;
     }
   }
 }
