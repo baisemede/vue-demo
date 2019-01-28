0d0ee9e49c7a25e3c612ec21b3f304a8d0afd22f
sujimao <1046975490@qq.com>
2019-01-28 18:12:21 +0800

    添加 click 事件

--- src/app.js
+++ src/app.js
@@ -7,5 +7,9 @@ Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 
 new Vue({
-    el:'#app'
+    el:'#app',
+    data:{
+        loading1:false,
+        loading2:false
+    }
 })
--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,8 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
-    <g-icon class="loading" name='loading'></g-icon>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
+  @click="$emit('click')">
+    <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+    <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -10,21 +11,29 @@
 <script>
 export default {
   props: {
-    icon:{},
-    iconPosition:{
-      type:String,
-      default:'left',
-      validator(value){
-        return value==='left' || value==='right'
+    icon: {},
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        return value === "left" || value === "right";
       }
     }
   }
 };
 </script>
 <style lang='scss'>
-@keyframes spin{
-  0%{transform: rotate(0deg);}
-  100%{transform: rotate(360deg);}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 .g-button {
   font-size: var(--font--size);
@@ -63,9 +72,9 @@ export default {
       margin-right: 0;
       margin-left: 0.1em;
     }
-    .loading{
-      animation: spin 2.5s infinite linear;
-    }
+  }
+  .loading {
+    animation: spin 1.5s infinite linear;
   }
 }
 </style>
