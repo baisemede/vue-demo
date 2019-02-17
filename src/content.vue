934bd05d31d4e5e78bfbf390cc7f6e120bbe855f
sujimao <1046975490@qq.com>
2019-02-17 18:11:42 +0800

    添加简单动画

---
+++ src/content.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="content">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .content {
+    flex-grow: 1;
+    background-color: #DDD6F3;
+  }
+</style>
