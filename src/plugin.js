292ab243144754cf96ca053a8c2e002b66fa704a
sujimao <1046975490@qq.com>
2019-02-18 15:19:20 +0800

    实现 autoClose closeButton enableHtml 多行文字 等功能

--- src/plugin.js
+++ src/plugin.js
@@ -2,9 +2,11 @@ import Toast from './toast'
 
 export default {
   install (Vue, options) {
-    Vue.prototype.$toast = function (message) {
+    Vue.prototype.$toast = function (message,toastOptions) {
       let Constructor = Vue.extend(Toast)
-      let toast = new Constructor()
+      let toast = new Constructor({
+        propsData: toastOptions
+      })
       toast.$slots.default = [message] //必须放mount前面
       toast.$mount()
       document.body.appendChild(toast.$el)
