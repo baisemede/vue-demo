de4ebeb228a1a2b958051edc3c07a2a205c6c194
sujimao <1046975490@qq.com>
2019-02-19 12:23:38 +0800

    修复测试代码中 click 太快引发的 error

--- src/button-group.vue
+++ src/button-group.vue
@@ -4,17 +4,16 @@
   </div>
 </template>
 <script>
-
 export default {
-    name:"gulu-button-group",
-    mounted(){
-        for(let node of this.$el.children){
-            let name=node.nodeName.toLowerCase()
-            if(name!=='button'){
-                console.warn(`g-button-group 的子元素不是 g-button,你写的是${name}`)
-            }
-        }
+  name: "gulu-button-group",
+  mounted() {
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase();
+      if (name !== "button") {
+        console.warn(`g-button-group 的子元素不是 g-button,你写的是${name}`);
+      }
     }
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -24,8 +23,8 @@ export default {
 }
 .g-button {
   border-radius: 0em;
-  &:not(:first-child){
-      margin-left: -1px;      
+  &:not(:first-child) {
+    margin-left: -1px;
   }
   &:first-child {
     border-top-left-radius: var(--border-radius);
@@ -35,9 +34,9 @@ export default {
     border-top-right-radius: var(--border-radius);
     border-bottom-right-radius: var(--border-radius);
   }
-  &:hover{
-      position: relative;
-      z-index: 1;
+  &:hover {
+    position: relative;
+    z-index: 1;
   }
 }
 </style>
