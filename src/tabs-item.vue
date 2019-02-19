4f79b5db6ee4dc6d61b75165a3646b9ea1327af0
sujimao <1046975490@qq.com>
2019-02-19 16:10:29 +0800

    添加disabled 功能

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="xxx" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -25,7 +25,8 @@ export default {
   computed: {
     classes() {
       return {
-        active: this.active
+        active: this.active,
+        disabled: this.disabled
       };
     }
   },
@@ -35,24 +36,31 @@ export default {
     });
   },
   methods: {
-    xxx() {
-      this.eventBus.$emit('update:selected', this.name, this);
+    onClick() {
+      if (this.disabled) {
+        return;
+      }
+      this.eventBus.$emit("update:selected", this.name, this);
     }
   }
 };
 </script>
 <style lang="scss" scoped>
 $blue: #57bae8;
+$disabled-text-color: grey;
 .tabs-item {
-    flex-shrink: 0;
-    padding: 0 1em;
-    cursor: pointer;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    &.active {
-      color: $blue;
-      font-weight: bold;
-    }
+  flex-shrink: 0;
+  padding: 0 1em;
+  cursor: pointer;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  &.active {
+    color: $blue;
+    font-weight: bold;
   }
+  &.disabled {
+    color: $disabled-text-color;
+  }
+}
 </style>
