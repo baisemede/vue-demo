6121e4e0beaebf93cd5b6063d9e6282191b1f5ca
sujimao <1046975490@qq.com>
2019-02-20 12:15:21 +0800

    实现最简单的popver

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click="xxx">
-    <div class="content-wrapper" v-if="visible">
+  <div class="popover" @click.stop="xxx">
+    <div class="content-wrapper" v-if="visible" @click.stop> <!-- @click.stop 可以阻止事件冒泡!!! -->
       <slot name="content"></slot>
     </div>
     <slot></slot>
@@ -8,32 +8,39 @@
 </template>
 
 <script>
-  export default {
-    name: "gulu-Popover",
-    data () {
-      return {visible: false}
-    },
-    methods: {
-      xxx () {
-        this.visible = !this.visible
-      }
+export default {
+  name: "gulu-Popover",
+  data() {
+    return { visible: false };
+  },
+  methods: {
+    xxx() {
+      this.visible = !this.visible;
+      this.$nextTick(() => {
+        let eventHandler = () => {
+          this.visible = false;
+          document.removeEventListener("click", eventHandler); //删除当前函数，防止每次点击都新增！！！
+        };
+        //添加click事件，点击按钮外部触发 使popover 隐藏
+        document.addEventListener("click", eventHandler);
+      });
     }
   }
+};
 </script>
 
 <style scoped lang="scss">
-  .popover {
-    display: inline-block;
-    vertical-align: top;
-    position: relative;
-    .content-wrapper {
-      position: absolute;
-      background-color: #b9b9c8;
-      color: white;
-      bottom: 100%;
-      left: 0;
-      
-      box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    }
+.popover {
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
+  .content-wrapper {
+    position: absolute;
+    background-color: #b9b9c8;
+    color: white;
+    bottom: 100%;
+    left: 0;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
   }
+}
 </style>
