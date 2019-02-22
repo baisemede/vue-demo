2c4989d0765e38d17438bf628574ff1cc55a17fa
sujimao <1046975490@qq.com>
2019-02-22 14:45:18 +0800

    更新

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-import Button from './src/button'
-import Icon from './src/icon'
-import ButtonGroup from './src/button-group'
 
-export{ButtonGroup,Button,Icon}
+import Button from './button';
+import ButtonGroup from './button-group';
+import Col from './col';
+import Content from './content';
+import Footer from './footer';
+import Header from './header';
+import Icon from './icon';
+import Input from './input';
+import Layout from './layout';
+import Row from './row';
+import Sider from './sider';
+import Toast from './toast'
+import plugin from './plugin'
+import Tabs from './tabs'
+import TabsHead from './tabs-head'
+import TabsBody from './tabs-body'
+import TabsItem from './tabs-item'
+import TabsPane from './tabs-pane'
+import Popover from './popover'
+import CollapseItem from './collapse-item'
+import Collapse from './collapse'
+
+export{ButtonGroup,Button,Icon,Col,Content,Footer,Header,Input,Layout,Row,Sider,Toast,Tabs,TabsHead,TabsBody,TabsItem,
+    TabsItem,TabsPane,Popover,Popover,CollapseItem,Collapse,plugin}
