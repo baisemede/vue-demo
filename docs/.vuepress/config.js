d943e61cdc439975946e44f41454ddd1b79ebbf2
sujimao <1046975490@qq.com>
2019-03-01 13:33:53 +0800

    “更新”

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
     themeConfig:{
         nav:[
             {text:'主页',link:'/'},
-            {text:'文档',link:'/https://baisemede.github.io/vue-demo/'},
+            {text:'文档',link:'/introduce/'},
             {text:'Github',link:'https://github.com/baisemede/vue-demo'}
         ],
         sidebar: [{
