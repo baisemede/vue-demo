e7cdc52d470a82c8b62cb820de1998282e48d5d4
sujimao <1046975490@qq.com>
2019-03-01 10:14:56 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    base:'/vue-gulu-demo/',
+    base:'/vue-demo/',
     title: '轱辘UI',
     description: '轱辘 UI 是一个好用的 UI 框架',
     themeConfig:{
         nav:[
             {text:'主页',link:'/'},
-            {text:'文档',link:'/guide/'},
-            {text:'交流',link:'https://google.com'}
+            {text:'文档',link:'/https://baisemede.github.io/vue-demo/'},
+            {text:'Github',link:'https://github.com/baisemede/vue-demo'}
         ],
         sidebar: [{
                     title: '入门',
